9118301a25b44b95648b81fca9999a577ef03dd4
Chauncey2 <gaochangju95@163.com>
2021-09-14 16:10:14 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:Chauncey2/Chauncey2.github.io.git master
+git push -f git@Chauncey2.github.com:Chauncey2/Chauncey2.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
